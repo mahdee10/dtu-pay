8d293169544d86c087e2a8bc0638c285efe7f603
Ionut Eusebiu Andrici <ionut.andrici2@mail.dcu.ie>
2025-01-21 19:25:45 +0100

    fix deployment

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,12 +3,12 @@
 set -e
 
 echo "Building the Maven project..."
+mvn -f ./messaging-utilities-3.4/pom.xml clean package
 mvn -f ./DTU-Pay-Server/pom.xml clean package
 mvn -f ./AccountManagement/pom.xml clean package
 mvn -f ./TokenManagement/pom.xml clean package
 mvn -f ./PaymentManagement/pom.xml clean package
 mvn -f ./ReportingManagement/pom.xml clean package
-mvn -f ./messaging-utilities-3.4/pom.xml clean package
 
 echo "Building and running Docker containers..."
 docker compose build
