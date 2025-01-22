389e21d79000e10592b4c15ee6ff38f0d316eac0
Ugne Adamo <120172293+uadamo@users.noreply.github.com>
2025-01-22 16:51:27 +0100

    refactoring path names

--- DTU-Pay-Server/src/main/java/resources/CustomerResource.java
+++ DTU-Pay-Server/src/main/java/resources/CustomerResource.java
@@ -5,9 +5,12 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import models.AccountEventMessage;
 import models.ReportingEventMessage;
+import models.TokenEventMessage;
 import models.dtos.CreateCustomerDto;
+import models.dtos.TokenRequestDto;
 import services.CustomerService;
 import services.ReportingService;
+import services.TokenService;
 
 import java.util.UUID;
 
@@ -15,7 +18,7 @@ import java.util.UUID;
 public class CustomerResource {
     CustomerService service = CustomerService.getService();
     ReportingService reportingService = ReportingService.getInstance();
-
+    TokenService tokenService = TokenService.getInstance();
 
     @POST
     @Path("/register")
@@ -72,6 +75,43 @@ public class CustomerResource {
         return Response.status(Response.Status.OK)
                 .entity(eventMessage.getPaymentList())
                 .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+
+    @POST
+    @Path("/tokens/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createTokens(TokenRequestDto tokenRequestDto) {
+        TokenEventMessage eventMessage = tokenService.createTokens(tokenRequestDto.getCustomerId(), tokenRequestDto.getNTokens());
+
+        if (eventMessage.getRequestResponseCode() != Response.Status.OK.getStatusCode()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(eventMessage.getExceptionMessage())
+                    .build();
+        }
+
+        return Response.status(Response.Status.OK)
+                .entity(eventMessage.getCreatedTokens())
+                .build();
+    }
+
+    @GET
+    @Path("/tokens/{customerId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTokens(@PathParam("customerId") UUID customerId) {
+        TokenEventMessage eventMessage = tokenService.getToken(customerId);
+
+        if (eventMessage.getRequestResponseCode() != Response.Status.OK.getStatusCode()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(eventMessage.getExceptionMessage())
+                    .build();
+        }
+
+        return Response.status(Response.Status.OK)
+                .entity(eventMessage.getTokenUUID())
                 .build();
     }
 
